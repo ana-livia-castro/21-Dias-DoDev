0980812fd72e3db12d19d9fa2a354d31039f6a8f
Ana Castro <castro.analivia@gmail.com>
2024-04-18 15:38:34 -0300

    operadores aritmeticos

--- Dia-3/script.js
+++ Dia-3/script.js
@@ -1,16 +1,27 @@
-/* let tipoTexto = "2";
-let tipoNumber = 2;
-let tipoBoolTexto = "true";
-let tipoBool = true;
- */
-let meuNome = "Ana Castro";
-console.log(meuNome);
-meuNome = "Ana Livia";
-console.log(meuNome);
+let numUm = 5;
+let numDois = 3;
+let soma = numUm + numDois;
+let sub = numUm - numDois;
+let multi = numUm * numDois;
+let divi = numUm / numDois;
+let mod = numUm % numDois;
 
-console.log("======== Constante ========");
-
-const meuNomeConst = "Ana Castro";
-console.log(meuNomeConst);
-meuNomeConst = "Ana Livia";
-console.log(meuNomeConst);
+console.log("A soma dos numeros:", numUm, "e", numDois, "é igual = ", soma);
+console.log("A subtração dos numeros:", numUm, "e", numDois, "é igual = ", sub);
+console.log(
+  "A multiplicação dos numeros:",
+  numUm,
+  "e",
+  numDois,
+  "é igual = ",
+  multi
+);
+console.log("A divisão dos numeros:", numUm, "e", numDois, "é igual = ", divi);
+console.log(
+  "O resto da divisão dos numeros:",
+  numUm,
+  "e",
+  numDois,
+  "é igual = ",
+  mod
+);
