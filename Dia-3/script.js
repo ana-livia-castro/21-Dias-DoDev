8e4be626206e8349f9da212b250d969cca1b83a3
Ana Castro <castro.analivia@gmail.com>
2024-04-18 15:09:41 -0300

    let e const

--- Dia-3/script.js
+++ Dia-3/script.js
@@ -1,4 +1,16 @@
-let tipoTexto = "2";
+/* let tipoTexto = "2";
 let tipoNumber = 2;
 let tipoBoolTexto = "true";
 let tipoBool = true;
+ */
+let meuNome = "Ana Castro";
+console.log(meuNome);
+meuNome = "Ana Livia";
+console.log(meuNome);
+
+console.log("======== Constante ========");
+
+const meuNomeConst = "Ana Castro";
+console.log(meuNomeConst);
+meuNomeConst = "Ana Livia";
+console.log(meuNomeConst);
