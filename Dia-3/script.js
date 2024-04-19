bc15118f880bd4f99ec62875e14bee0c36af5b3a
Ana Castro <castro.analivia@gmail.com>
2024-04-19 09:02:43 -0300

    conhecendo prompt() e parseInt()

--- Dia-3/script.js
+++ Dia-3/script.js
@@ -1,27 +1,4 @@
-let numUm = 5;
-let numDois = 3;
-let soma = numUm + numDois;
-let sub = numUm - numDois;
-let multi = numUm * numDois;
-let divi = numUm / numDois;
-let mod = numUm % numDois;
-
-console.log("A soma dos numeros:", numUm, "e", numDois, "é igual = ", soma);
-console.log("A subtração dos numeros:", numUm, "e", numDois, "é igual = ", sub);
-console.log(
-  "A multiplicação dos numeros:",
-  numUm,
-  "e",
-  numDois,
-  "é igual = ",
-  multi
-);
-console.log("A divisão dos numeros:", numUm, "e", numDois, "é igual = ", divi);
-console.log(
-  "O resto da divisão dos numeros:",
-  numUm,
-  "e",
-  numDois,
-  "é igual = ",
-  mod
-);
+let entradaDoUsuario = prompt("insira sua idade");
+console.log(entradaDoUsuario);
+let entradaDoUsuarioEmInt = parseInt(entradaDoUsuario);
+console.log(entradaDoUsuarioEmInt);
