ccd20bb696ace9bb6ce9a381fb6b4fb2ed6f20b9
Ana Castro <castro.analivia@gmail.com>
2024-04-19 09:11:58 -0300

    um pequeno exemplo do dia-3

--- Dia-3/script.js
+++ Dia-3/script.js
@@ -1,4 +1,12 @@
-let entradaDoUsuario = prompt("insira sua idade");
-console.log(entradaDoUsuario);
-let entradaDoUsuarioEmInt = parseInt(entradaDoUsuario);
-console.log(entradaDoUsuarioEmInt);
+// CRIANDO VARIÁVEIS
+var texto = prompt("Digite um texto");
+var numero = parseInt(prompt("Digite um número"));
+var continuar = true;
+//ATRIBUINDO NOVOS VALORES ÀS VARIÁVEIS
+texto = "outro texto";
+numero = 2;
+continuar = false;
+//OPERAÇÕES
+numero = 2 + 2; //4
+numero = 10 % 2; //0
+console.log("Um texto e " + texto); // "Um texto e outro texto"
