b15ab678a0fb68e0ebe567d2b52ffa4da6298ae6
Ana Castro <castro.analivia@gmail.com>
2024-04-24 08:01:17 -0300

    condicional IF e ELSE

---
+++ Dia-4/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("Insira sua idade:"));
+// == igual | != diferente | < menor que e > maior que | <= >= menor e maior ou igual
+// ===  é igual o valor e o tipo | !==  é diferença o valor  e o tipo
+if (idade <= 10) {
+  console.log("Você tem menos ou tem 10 anos");
+} else {
+  console.log("Você tem mais que 10 anos");
+}
