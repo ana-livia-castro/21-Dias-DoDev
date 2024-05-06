6d81634ecccbc1ba4fe1eee8c66ccc8ee7d575d2
Ana Castro <castro.analivia@gmail.com>
2024-05-06 14:50:19 -0300

    switch case

---
+++ Dia-5/desafio1.js
@@ -0,0 +1,24 @@
+let num1 = parseInt(prompt("Insira o primeiro número:"));
+let num2 = parseInt(prompt("Insira o segundo número:"));
+let opcao = parseInt(
+  prompt(
+    "Escolha a operação  que deseja realizar:" +
+      "\n1 = +; \n2 = -; \n3 = *; \n4 = /"
+  )
+);
+
+//Criando SwitchCase
+switch (opcao) {
+  case 1:
+    console.log(num1 + " + " + num2 + " = " + (num1 + num2));
+    break;
+  case 2:
+    console.log(num1 + "-" + num2 + "=" + (num1 - num2));
+    break;
+  case 3:
+    console.log(num1 + "*" + num2 + "=" + num1 * num2);
+    break;
+  default:
+    console.log(num1 + "/" + num2 + "=" + num1 / num2);
+    break;
+}
